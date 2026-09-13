a7ba9cf38f7ece3a69d29b40a9172825be16c503
Muhammad Subhan <186476709+Developer359@users.noreply.github.com>
2026-09-13 16:10:08 +0500

    testing and ad new keywords

--- Core/job-search.py
+++ Core/job-search.py
@@ -40,7 +40,7 @@ CATEGORY_ROLE_KEYWORDS = {
     "Frontend, Full Stack & Backend": [
         "frontend", "front-end", "front end", "backend", "back-end", "back end",
         "full stack", "fullstack", "full-stack", "web developer", "java",
-        "Html", "react", "next.js", "nextjs", "node", "javascript", "typescript",".net", "ruby", "php", "c#", "c++", "golang", "go"
+        "Html", "css", "react", "next.js", "nextjs", "node", "javascript", "typescript",".net", "ruby", "php", "c#", "c++", "golang", "go"
     ],
     "AI & Data Engineer": [
         "ai", "artificial intelligence", "machine learning", "ml engineer", "data scientist",
